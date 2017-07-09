795e30732905d57a12953a863e754cf8a8ebee52
이주영 <stylejy@gmail.com>
2017-07-10 00:22:38 +0100

    Refined OsmParser

--- src/main/scala/com/github/stylejy/app/OsmParser.scala
+++ src/main/scala/com/github/stylejy/app/OsmParser.scala
@@ -18,116 +18,106 @@
 package com.github.stylejy.app
 
 import scala.math._
-import java.io.File
-import scala.xml.XML
-import java.io.FileOutputStream
-import java.io.DataOutputStream
+import java.io.{DataOutputStream, File, FileInputStream, FileOutputStream}
+
+import scala.xml.{Elem, XML}
 import scala.collection.mutable.Map
 import scala.collection.mutable.ArrayBuffer
 
 object OsmParser {
 
-  var nodes: Map[Long, Node] = null
-  var edges: ArrayBuffer[Edge] = null
+  var nodes: Map[Long, Node] = Map()
+  var edges: ArrayBuffer[Edge] = ArrayBuffer()
 
   case class Node(lat:Float, lon:Float)
   case class Edge(from:Long, to:Long, dist:Int)
 
-  def Long2Node(id:Long): Node =
-    nodes.get(id) match {
-      case Some(node) => node
-      case None => Node(0f,0f)
-    }
-
-  def run {
+  def run = {
     println("usage: parse <file>.osm")
     val start = System.currentTimeMillis()
     parse("osmdata/sample.osm")
     println(((start-System.currentTimeMillis())/60000)+"min")
   }
 
-  def dataOutputStream(file: String)
-  = new DataOutputStream(new FileOutputStream(new File(file)))
-
-
-  def dist(from: Node, to: Node): Int = {
-    val lat1 = toRadians(from.lat)
-    val lon1 = toRadians(from.lon)
-    val lat2 = toRadians(to.lat)
-    val lon2 = toRadians(to.lon)
-
-
-    ((6378.388f * acos(
-      sin(lat1) * sin(lat2)
-        + cos(lat1) * cos(lat2)
-        * cos(lon2 - lon1)))*1000).toInt
-  }
-
   def parse(osm_file: String) {
 
     val xml = XML.loadFile(osm_file)
-    edges = ArrayBuffer[Edge]()
-    nodes = Map[Long, Node]()
 
+    readNodes(xml)
+    readWays(xml)
+    sortEdges
+    writeFiles
+  }
 
-    print("\n -> reading nodes..")
+  def readNodes(xml: Elem) = {
+    println("\n -> reading nodes..")
+    println(xml)
     (xml \ "node") foreach { (node) =>
 
-        val id = (node\"@id").text.toLong
-        val lat = (node\"@lat").text.toFloat
-        val lon = (node\"@lon").text.toFloat
-        nodes(id) = Node(lat, lon)
-        println("***PARSER*** OSM ID: " + id + " Latitude: " + lat + " Longitude: " + lon)
+      val id = (node\"@id").text.toLong
+      val lat = (node\"@lat").text.toFloat
+      val lon = (node\"@lon").text.toFloat
+      nodes(id) = Node(lat, lon)
+      println("***PARSER*** OSM ID: " + id + " Latitude: " + lat + " Longitude: " + lon)
     }
     println("   ("+nodes.size+") Done.")
+  }
 
+  def readWays(xml: Elem) = {
+    println(" -> reading ways...")
 
-    print(" -> reading ways...")
     (xml \ "way") foreach { (way) =>
 
-        val speed = ((way\"tag" filter { (t) => (t\"@k").text == "highway" })
-                                                 \ "@v").text match {
-          case "secondary_link" => 30 // km/h
-          case "motorway_link" => 50 // km/h
-          case "living_street" => 30
-          case "unclassified" => 30
-          case "primary_link" => 30
-          case "residential" => 20
-          case "trunk_link" => 30
-          case "secondary" => 60
-          case "motorway" => 90
-          case "tertiary" => 40
-          case "service" => 30
-          case "primary" => 80
-          case "track" => 30
-          case "trunk" => 50
-          case "road" => 50
-          case "path" => 10
-          case "steps" => 0
-          case "footway" => 10
-          //case "cycleway" => 20
-          case "pedestrian" => 10
-          //case (hw:String) if (!hw.equals("")) => println("highway "+hw); 10
-          case _ => 0
-        }
+      val speed = ((way\"tag" filter { (t) => (t\"@k").text == "highway" })
+        \ "@v").text match {
+        case "secondary_link" => 30 // km/h
+        case "motorway_link" => 50 // km/h
+        case "living_street" => 30
+        case "unclassified" => 30
+        case "primary_link" => 30
+        case "residential" => 20
+        case "trunk_link" => 30
+        case "secondary" => 60
+        case "motorway" => 90
+        case "tertiary" => 40
+        case "service" => 30
+        case "primary" => 80
+        case "track" => 30
+        case "trunk" => 50
+        case "road" => 50
+        case "path" => 10
+        case "steps" => 0
+        case "footway" => 10
+        //case "cycleway" => 20
+        case "pedestrian" => 10
+        //case (hw:String) if (!hw.equals("")) => println("highway "+hw); 10
+        case _ => 0
+      }
 
-        if (speed > 0) {
-          val ids = way\"nd" map { (nd) => ((nd\"@ref").text.toLong)}
-          for ((u,v) <- ids zip ids.tail) {
-            edges += Edge(u, v, dist(Long2Node(u),Long2Node(v)))
-            edges += Edge(v, u, dist(Long2Node(v),Long2Node(u)))
-          }
+      if (speed > 0) {
+        val ids = way\"nd" map { (nd) => ((nd\"@ref").text.toLong)}
+        for ((u,v) <- ids zip ids.tail) {
+          edges += Edge(u, v, dist(Long2Node(u),Long2Node(v)))
+          edges += Edge(v, u, dist(Long2Node(v),Long2Node(u)))
         }
+      }
     }
+
     println("   ("+edges.size+") Done.")
+  }
 
+  def sortEdges = {
+    println(" -> sorting edges..")
 
-    print(" -> sorting edges..")
     edges = edges.sortWith(_.from < _.from)
+
     println("          Done.")
+  }
 
 
+  def writeFiles = {
     println(" -> writing file...")
+
     val osm_id_map = Map[Long, Long]()
     var edge_buf = ArrayBuffer[Long]()
     val node_out = dataOutputStream("nodes.bin")
@@ -168,4 +158,25 @@ object OsmParser {
 
     println("          Done.\n")
   }
+
+  def dataOutputStream(file: String) = new DataOutputStream(new FileOutputStream(new File(file)))
+
+  def dist(from: Node, to: Node): Int = {
+    val lat1 = toRadians(from.lat)
+    val lon1 = toRadians(from.lon)
+    val lat2 = toRadians(to.lat)
+    val lon2 = toRadians(to.lon)
+
+
+    ((6378.388f * acos(
+      sin(lat1) * sin(lat2)
+        + cos(lat1) * cos(lat2)
+        * cos(lon2 - lon1)))*1000).toInt
+  }
+
+  def Long2Node(id:Long): Node =
+    nodes.get(id) match {
+      case Some(node) => node
+      case None => Node(0f,0f)
+    }
 }
