320bf8a354a315c46c9d65c8124e69a624b07c0e
이주영 <stylejy@gmail.com>
2017-07-24 00:00:16 +0100

    Get coordinates and leave markers.

--- src/main/webapp/assets/js/LeafletController.js
+++ src/main/webapp/assets/js/LeafletController.js
@@ -1,5 +1,70 @@
+var coordinates;
+var marker = [];
+
+var isSourceFixed = false;
+var isTargetFixed = false;
+var isMarkerCreated = [false, false];
+
 function onMapClick(e) {
-    document.getElementById("source").innerHTML = e.latlng;
+    coordinates = e.latlng;
+    markerController();
+    choosePoints();
 }
 
 mymap.on('click', onMapClick);
+
+function choosePoints() {
+    if (isSourceFixed == false) {
+        document.getElementById("source").innerHTML = 'Chosen Source Point -> ' + shapeCoordinates()
+            + selectPointButton("sourceBtn");
+        document.getElementById("sourceBtn").addEventListener("click", clickSourceButton);
+    } else if (isTargetFixed == false) {
+        document.getElementById("target").innerHTML = 'Chosen Target Point -> ' + shapeCoordinates() + selectPointButton("targetBtn");
+        document.getElementById("targetBtn").addEventListener("click", clickTargetButton);
+    }
+}
+
+function selectPointButton(element) {
+    return '&nbsp&nbsp&nbsp<button type=button id="' + element + '" class="btn btn-success btn-xs">Select Point</button>';
+}
+
+function shapeCoordinates() {
+    return '( Latitude: ' + coordinates.lat + ' )&nbsp&nbsp&nbsp( Longitude: ' + coordinates.lng + ' )';
+}
+
+function clickSourceButton() {
+    isSourceFixed = true;
+    document.getElementById("source").innerHTML
+        = shapeCoordinates() + '&nbsp&nbsp&nbsp<button type=button id=changeSource class="btn btn-warning btn-xs">Source Point Fixed</button>';
+}
+
+function clickTargetButton() {
+    isTargetFixed = true;
+    document.getElementById("target").innerHTML
+        = shapeCoordinates() + '&nbsp&nbsp&nbsp<button type=button id=changeTarget class="btn btn-warning btn-xs">Target Point Point</button>';
+}
+
+function markerController() {
+    var switcher;
+
+    if (isSourceFixed == false || isTargetFixed == false) {
+        if (isSourceFixed == false) {
+            switcher = 0;
+        } else {
+            switcher = 1;
+        }
+
+        if (isMarkerCreated[switcher] == false) {
+            marker[switcher] = L.marker([coordinates.lat, coordinates.lng]);
+            marker[switcher].addTo(mymap);
+            isMarkerCreated[switcher] = true;
+        } else {
+            marker[switcher].setLatLng([coordinates.lat, coordinates.lng]);
+        }
+        if (switcher == 0) {
+            marker[switcher].bindPopup("<b>For the source point</b><br>Press Select Point button!!").openPopup();
+        } else {
+            marker[switcher].bindPopup("<b>For the target point</b><br>Press Select Point button!!").openPopup();
+        }
+    }
+}
